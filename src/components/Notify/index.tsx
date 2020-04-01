3c6f2b0f434a5a35036690c9a96bdaae9150eef1
fremango <boping2010@gmail.com>
2020-04-01 12:11:25 +0800

    update Notify loading style

--- src/components/Notify/index.tsx
+++ src/components/Notify/index.tsx
@@ -1,7 +1,12 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+
+import { checkDicomParseProgress } from "_helper";
 
 import "./style.less";
-import { checkDicomParseProgress } from "_helper";
+
+const antIcon = <LoadingOutlined style={{ fontSize: 18 }} spin />;
 
 interface NotifyPropsI {
   mode: "parsing" | "successed";
@@ -10,28 +15,38 @@ interface NotifyPropsI {
 }
 
 const Notify: FunctionComponent<NotifyPropsI> = (props) => {
-  const { mode, onClose, onChange } = props;
+  const { mode = "parsing", onClose, onChange } = props;
+  const [isLoading, setIsLoading] = useState(false);
 
   const onClick = (): void => {
     if (mode === "successed" && onClose) onClose();
-    else
+    else {
+      setIsLoading(true);
       checkDicomParseProgress()
         .then((res) => {
           onChange && onChange(res);
+          setIsLoading(false);
         })
         .catch((err) => {
           console.error("pull parsing count error: ", err);
         });
+    }
   };
 
   return (
     <div className={`notify ${mode === "parsing" ? "notify-parsing" : ""}`}>
       <i className="iconfont iconic_info"></i>
       <span className="notify-text">{props.children}</span>
-      <i
-        className={`notify-ctl iconfont ${mode === "parsing" ? "iconic_refresh" : "iconic_close1"}`}
-        onClick={onClick}
-      ></i>
+      {isLoading ? (
+        <Spin indicator={antIcon} />
+      ) : (
+        <i
+          className={`notify-ctl iconfont ${
+            mode === "parsing" ? "iconic_refresh" : "iconic_close1"
+          }`}
+          onClick={onClick}
+        ></i>
+      )}
     </div>
   );
 };
